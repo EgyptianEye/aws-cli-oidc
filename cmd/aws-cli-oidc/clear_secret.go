dc9b6e913af606049b1eec40a97fc07758551be3
horus <horus.li@gmail.com>
2022-04-18 17:46:51 +0800

    Update clear-secret subcommand
    Note: using rm(1) instead is highly recommended

--- cmd/aws-cli-oidc/clear_secret.go
+++ cmd/aws-cli-oidc/clear_secret.go
@@ -1,25 +1,36 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/openstandia/aws-cli-oidc/lib"
 	"github.com/spf13/cobra"
 )
 
 var clearSecretCmd = &cobra.Command{
 	Use:   "clear-secret",
-	Short: "Clear OS secret store that saves AWS credentials",
-	Long:  `Clear OS secret store that saves AWS credentials.`,
+	Short: "Clear secret store that saves AWS credentials",
+	Long:  `Clear secret store that saves AWS credentials.`,
 	Run:   clearSecret,
 }
 
 func init() {
+	clearSecretCmd.Flags().StringP("provider", "p", "", "OIDC provider name")
+	clearSecretCmd.Flags().StringP("use-secret", "s", "", "AWS credentials store: [keyring] or [file]")
 	rootCmd.AddCommand(clearSecretCmd)
 }
 
 func clearSecret(cmd *cobra.Command, args []string) {
-	if err := lib.Clear(); err != nil {
-		lib.Writeln("Failed to clear the secret store")
-		lib.Exit(err)
+	provider, _ := cmd.Flags().GetString("provider")
+	secret, _ := cmd.Flags().GetString("use-secret")
+	if provider == "" || secret == "" {
+		lib.Exit(cmd.Usage())
 	}
-	lib.Write("The secret store has been cleared")
+	if err := lib.InitializeSecret(secret, provider); err != nil {
+		lib.Exit(fmt.Errorf("cannot initialize store: %w", err))
+	}
+	if err := lib.ClearSecret(); err != nil {
+		lib.Exit(fmt.Errorf("failed to clear the secret store: %w", err))
+	}
+	lib.Writeln("The secret store has been cleared")
 }
